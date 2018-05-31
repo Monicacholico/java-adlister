c4774bac936e0290ed97e489937a0927c9462cc9
monicacholico <monicachol@hotmail.com>
2018-05-31 12:00:12 -0500

    added form to pass a name

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,14 +4,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Parameter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-
-@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+
+    private String message;
+
+    public void init(){
+        message = "Hello Monica, you're not awesome at coding!";
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+
+//        String paramName = "Monica!";
+//        String paramValue = request.getParameter(paramName);
+//        out.write(paramName);
+//        out.write(" = ");
+//        out.write(paramValue);
+//        out.write("Hello World");
+//
+//        paramName = "UNKNOWN";
+//        paramValue = request.getParameter(paramName);
+//
+//        if(paramValue==null){
+//            out.write("Parameter " + paramName + " not found");
+//        }
+//
+//        out.close();
+
+        out.println("<h1>" + message + "</h1>");
+
+        String name = request.getParameter("name");
+
+        if (name == null){
+            name = "World";
+        }
+
+
+        response.getWriter().println("<h1 style =\"font-family: fantasy;\">Goodbye " + name + " !</h1>");
+        response.getWriter().println("<p>Enter your name:</p>");
+        response.getWriter().println("<form action=\"/hello\">");
+        response.getWriter().println("<input name=\"name\"/>");
+        response.getWriter().println("</form>");
+        
     }
 }
+
+
---
+++ src/main/java/SimpleCounter.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+
+public class SimpleCounter extends HttpServlet {
+    int counter = 0;
+    public void doGet(HttpServletRequest request, HttpServletResponse
+            response)throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        counter++;
+        pw.println("At present the value of the counter is " + counter);
+    }
+}
