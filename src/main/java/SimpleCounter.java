8defe6937fc15ecbb5c42f35cfb8727fa30e2922
monicacholico <monicachol@hotmail.com>
2018-05-31 15:43:23 -0500

    last commit

--- src/main/java/SimpleCounter.java
+++ src/main/java/SimpleCounter.java
@@ -1,20 +1,25 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 
 
 
+@WebServlet(name = "SimpleCounter", urlPatterns = "/count")
 public class SimpleCounter extends HttpServlet {
-    int counter = 0;
+
+    protected int counter = 0;
+
+
     public void doGet(HttpServletRequest request, HttpServletResponse
             response)throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter pw = response.getWriter();
+
         counter++;
-        pw.println("At present the value of the counter is " + counter);
+
+        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+
     }
 }
